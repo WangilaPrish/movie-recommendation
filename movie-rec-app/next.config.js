a266368ec0012e38291023bcfc67c261ef3b7ae4
WangilaPrish <nabwonyaprish@gmail.com>
2025-10-06 16:01:13 +0300

    refactor: update package.json for dependency cleanup and version alignment

---
+++ movie-rec-app/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['image.tmdb.org', 'www.themoviedb.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
+}
+
+module.exports = nextConfig
